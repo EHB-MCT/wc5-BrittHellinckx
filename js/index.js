2df4f43627589dd4cb8dc1bcead578e5bdd35f3f
Britt Hellinckx <britt.hellinckx@student.ehb.be>
2021-04-20 16:13:40 +0200

    Oplossen WC5

--- js/index.js
+++ js/index.js
@@ -2,14 +2,53 @@
 
 const messageSystem = {
   startFetching() {
+    document.getElementById('messageForm').addEventListener('submit');
+    setInterval(this.fetchMessages, 1000);
   },
 
   sendMessage(msg) {
     // https://thecrew.cc/api/message/create.php?token=__TOKEN__ POST
+    fetch('https://thecrew.cc/api/message/create.php?token=' + userSystem.token, {
+      method: 'POST',
+      body: JSON.stringify({ message: msg})
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        this.fetchMessages();
+      });
   },
 
   fetchMessages() {
     // https://thecrew.cc/api/message/read.php?token=__TOKEN__ GET
+    fetch('https://thecrew.cc/api/message/read.php?token=' + userSystem.token, {
+        method: 'GET'
+      })
+      .then(response => response.json())
+      /* if (status == 201) {
+                  console.error(data);
+                } else if (status == 200) {}*/
+      .then((data) => {
+        data.sort((a, b) => b.ID - a.ID);
+        const outputField = document.getElementById('output');
+        outputField.innerHTML = "";
+
+        if (data.length == 0) {
+          outputField.innerHTML = "No messages";
+        } else {
+          data.forEach(item => {
+            const outputMessage = //eventuuel toevoegen id="message-${item.ID}"
+              `<div class="message">
+            <span class="by">${item.handle}</span>
+            <span class="on">${item.created_at}</span
+            ><p>${item.message}</p>
+            </div>`;
+            outputField.insertAdjacentHTML("afterbegin", outputMessage);
+          });
+        }
+      });
   }
 };
 
@@ -17,6 +56,14 @@ const userSystem = {
   token: "",
   loggedIn: false,
 
+  checkToken() {
+    const localToken = this.getToken();
+    if (localToken !== null) {
+      this.token = localToken;
+      messageSystem.fetchMessages();
+      document.getElementById('loginWindow').style.display = 'none';
+    }
+  },
   saveToken() {
     localStorage.setItem("token", this.token);
   },
@@ -25,22 +72,64 @@ const userSystem = {
     return localStorage.getItem("token");
   },
 
-  logout() { 
+  logout() {
     localStorage.removeItem("token");
+    location.reload();
   },
 
   login(email, password) {
     // https://thecrew.cc/api/user/login.php POST
+    fetch('https://thecrew.cc/api/user/login.php', {
+        method: 'POST',
+        body: JSON.stringify({
+          email: email,
+          pasword: password
+        })
+        //kan ook: body: JSON.stringify{email, password}
+      })
+      .then(response => response.json())
+      .then((data) => {
+        const token = data.token;
+        this.token = token;
+        this.saveToken();
+        messageSystem.fetchMessages();
+
+        document.getElementById('loginWindow').style.display = 'none';
+      });
   },
 
   updateUser(password, handle) {
     // https://thecrew.cc/api/user/update.php?token=__TOKEN__ POST
+    fetch(`https://thecrew.cc/api/user/update.php?token=`+ userSystem.token, {
+      method: 'POST',
+      body: JSON.stringify({ handle, password})
+    })
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+      });
   }
 };
 
 const display = {
   initFields() {
+    document.getElementById('loginForm').addEventListener('submit', (e) => {
+      e.preventDefault();
+      const email = document.getElementById('emailField').value;
+      const password = document.getElementById('passwordField').value;
+      userSystem.login(email, password);
+    });
+    document.getElementById('logoutBtn').addEventListener('click', userSystem.logout);
+    document.getElementById('messageForm').addEventListener('submit', (e) => {
+      e.preventDefault();
+      const messageField =document.getElementById('messageField');
+      messageSystem.sendMessage(messageField.value);
+      messageField.value="";
+    });
   }
 };
 
-  display.initFields();
+display.initFields();
+userSystem.checkToken();
